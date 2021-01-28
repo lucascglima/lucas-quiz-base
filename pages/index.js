da2bdd8057e9679859afc014a219017b1d695241
Lucas Lima <66095249+lucascglima@users.noreply.github.com>
2021-01-28 12:25:52 -0300

    Base projeto react

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  return (
+  
+    <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <Widget>
+    
+            <Widget.Header>
+                <h1> The Quiz Of Football</h1>
+            </Widget.Header>
+
+
+            <Widget.Content>
+              <p> Texto do quiz</p>
+            </Widget.Content>
+  
+          </Widget>
+
+          <Widget>
+
+    <Widget.Content>
+    <h1> The Quiz Of Football</h1>
+    <p> The Quiz Of Football</p>
+    </Widget.Content>
+    </Widget>
+
+    <Footer/>
+      </QuizContainer>
+
+      <GitHubCorner projectUrl="https://github.com/lucascglima"/>
+
+    </QuizBackground>
+    
+  
+    )
+}
+  
